78092c3fe2b73393a74d4c56ca75856109973ee3
RSuryaa <105858699+RSuryaa@users.noreply.github.com>
2023-09-21 16:54:50 +0530

    Create Receiver.ino
    My receiver sketch

---
+++ Receiver.ino
@@ -0,0 +1,42 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+RH_ASK driver;
+
+const uint8_t maxMessageSize = 50; // Maximum message size
+
+uint8_t buf[maxMessageSize];
+uint8_t buflen;
+
+void setup() {
+  Serial.begin(9600);    // Debugging only
+  buflen = sizeof(buf); // Initialize buflen to the size of the buffer
+  if (!driver.init())
+    Serial.println("init failed");
+  else
+    Serial.println("init success");
+}
+
+void loop() {
+  if (driver.recv(buf, &buflen)) // Non-blocking
+  {
+    // Message with a good checksum received, dump it.
+    Serial.print("Message: ");
+    for (int i = 0; i < buflen; i++) {
+      if (isPrintableChar(buf[i])) {
+        Serial.write(buf[i]);
+      }
+      else {
+        Serial.print("Non-printable character (ASCII code ");
+        Serial.print(buf[i]);
+        Serial.print(")");
+      }
+    }
+    Serial.println();
+    // Reset buflen to the size of the buffer for the next message
+    buflen = sizeof(buf);
+  }
+}
+
+bool isPrintableChar(uint8_t c) {
+  return (c >= 32 && c <= 126); // ASCII printable characters
+}
