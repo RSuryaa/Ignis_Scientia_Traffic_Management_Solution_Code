6cc82173f08f99f4943bbb9efe4bda1b5e20d3c1
RSuryaa <105858699+RSuryaa@users.noreply.github.com>
2023-09-21 16:52:20 +0530

    Create Transmitter_v1.0.ino
    My transmitter sketch

---
+++ Transmitter_v1.0.ino
@@ -0,0 +1,25 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);	  // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+char Lol[30]="ABCDEFGHI";
+char Haha[30]="Hey dude, how are you?";
+void loop()
+{
+  transmit(Lol);
+  transmit(Haha);
+}
+void transmit(char *data)
+{
+    const char *msg = data;
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(100);
+}
